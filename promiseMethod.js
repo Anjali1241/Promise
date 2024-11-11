8aa3b51274f43c2a91001192d2b8699156d33ccd
Anjali Sharma <sharmaanjali1241@gmail.com>
2024-11-12 01:59:05 +0530

    Promsie.allSettled method

--- promiseMethod.js
+++ promiseMethod.js
@@ -14,7 +14,7 @@
 // Note: Even though the intermediate promise gets rejected, all next promises will not be stopped from executing. They will all be executed – but only the first rejected promise value will be available in the error parameter of the catch block.
 // example:
 const promise1=new Promise((resolve)=>resolve("promise one"))
-const promise2=new Promise((resolve)=>resolve("promise two"))
+const promise2=new Promise((resolve,reject)=>reject("promise two reject"))
 const promise3=new Promise((resolve)=>resolve("promise three"))
 
 Promise.all([promise1, promise2, promise3])
@@ -34,6 +34,19 @@ Promise.all([promise1, promise2, promise3])
 // 2. If the first promise in the promise chain gets rejected, the .catch handler will be executed and the result will be the result of the first failed promise.
 // 3. If multiple promises are rejected, the .catch handler will be executed and the result will be the result of the first failed promise.
   Promise.race([promise1, promise2, promise3])
+  .then((result) => {
+    console.log('resolved', result); // resolved promise one
+  })
+  .catch((error) => {
+    console.log('rejected', error);
+  });
+
+  // Promise.allSettled Method
+  //  Promise.allSettled we can get the result of all the promises, even if they failed.
+  //  It returns an array of objects, each of which contains a status and a value.
+
+  // Example:
+  Promise.allSettled([promise1, promise2, promise3])
   .then((result) => {
     console.log('resolved', result); // resolved promise one
   })
